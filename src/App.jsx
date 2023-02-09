a01011bedb00b0c87991e9f1cdc350c2aafb670b
Tanvir Hossan <tanvirhossan27@gmail.com>
2023-02-09 22:51:40 +0600

    added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Banner from "./components/Banner/Banner";
 import Deals from "./components/Deals/Deals";
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Thing from "./components/Thing/Thing";
 
@@ -10,6 +11,7 @@ function App() {
       <Banner />
       <Thing />
       <Deals />
+      <Footer />
     </>
   );
 }
